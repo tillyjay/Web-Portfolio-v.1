4ebe7ab546aa3d9529b3a7286369b33ca2e4a53e
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-17 10:20:07 -0300

    - fixed: consolidated snap functions for mobile pages

--- assets/js/contactSnap.js
+++
@@ -1,9 +0,0 @@
-// if mobile site surpasses certain size, snap to desktop version
-
-window.onresize = function indexSnap() {
-    if ($(window).width() > 650) {
-        window.location = "/index.html#contactSection"
-    }
-  }
-
-indexSnap();
---
+++ assets/js/mobileSnap.js
@@ -0,0 +1,57 @@
+//detect current page using window.location.pathname
+const currentPage = window.location.pathname;
+
+
+//about Snap
+const aboutSnap = () => {
+    if ($(window).width() > 650) {
+        window.location = "/index.html#aboutSection";
+    }
+};
+
+//contact Snap
+const contactSnap = () => {
+    if ($(window).width() > 650) {
+        window.location = "/index.html#contactSection";
+    }
+};
+
+//resume Snap
+const resumeSnap = () => {
+    if ($(window).width() > 650) {
+        window.location = "/index.html#resumeSection";
+    }
+};
+
+//work Snap
+const workSnap = () => {
+    if ($(window).width() > 650) {
+        window.location = "/index.html#workSection";
+    }
+};
+
+
+//check which page is currently open and apply right snap
+window.onresize = () => {
+    if (currentPage.includes('aboutMe.html')) {
+        aboutSnap();
+    } else if (currentPage.includes('contact.html')) {
+        contactSnap();
+    } else if (currentPage.includes('resume.html')) {
+        resumeSnap();
+    } else if (currentPage.includes('work.html')) {
+        workSnap();
+    } 
+};
+
+
+//initial check when page loads
+if (currentPage.includes('aboutMe.html')) {
+    aboutSnap();
+} else if (currentPage.includes('contact.html')) {
+    contactSnap();    
+} else if (currentPage.includes('resume.html')) {
+    resumeSnap();
+} else if (currentPage.includes('work.html')) {
+    workSnap();
+} 
