e39bbf71f49183ed70f5110706a6655bb9ec0a46
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-17 11:15:49 -0300

    - added footer date dynamically

---
+++ assets/js/footerDate.js
@@ -0,0 +1,5 @@
+//get current year
+const currentYear = new Date().getFullYear();
+
+//select element "footerYear", update text content
+document.getElementById("footerYear").textContent = `tillyjay${currentYear}`;
