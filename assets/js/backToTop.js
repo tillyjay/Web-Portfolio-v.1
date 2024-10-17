8b43aac6c7a6737dfefd5a90807d14bddcb9b967
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-17 12:44:55 -0300

    - fixed: removed modal section from work ul card list

---
+++ assets/js/backToTop.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const backToTopBtn = document.getElementById("backToTopBtn");
+
+//     // Show the button when the user scrolls down
+//     window.addEventListener("scroll", function () {
+//         if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//             backToTopBtn.style.display = "block"; // Show button
+//         } else {
+//             backToTopBtn.style.display = "none"; // Hide button
+//         }
+//     });
+
+//     // Scroll to the top when the button is clicked
+//     backToTopBtn.addEventListener("click", function () {
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//     });
+// });
