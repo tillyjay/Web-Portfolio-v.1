7c8f1895c5e8c39f6f2084c57b9f7c34bdd7684b
tillyJay <tilly.jay.campbell@gmail.com>
2024-10-21 09:53:56 -0300

    - fix: removed device detect, updated sphere position

--- assets/js/detectDevice.js
+++
@@ -1,7 +0,0 @@
-const md = new MobileDetect(window.navigator.userAgent);
-
-if (md.mobile()) {
-    console.log('This is a mobile device.');
-} else {
-    console.log('This is a desktop device.');
-}
